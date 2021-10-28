4b72cd84ec9f33eca463978b19968448310ca9ee
Miguel Angel Hernandez Melo <miguel.hernandez1@arrivia.com>
2021-10-28 13:22:59 -0500

    first version of page components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'styles-project';
+export class AppComponent implements OnInit{
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { LandingComponent } from './landing/landing.component';
+import { PackagesComponent } from './packages/packages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FooterComponent } from './footer/footer.component';
+import { CustomersComponent } from './customers/customers.component';
+import { ModalComponent } from './Shared/modal/modal.component';
+import { ClickOutsideDirective } from './Shared/Directives/clickOutside.component';
+import { PokemonGridComponent } from './pokemon-grid/pokemon-grid.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    LandingComponent,
+    PackagesComponent,
+    FooterComponent,
+    CustomersComponent,
+    ModalComponent,
+    ClickOutsideDirective,
+    PokemonGridComponent,
+    SidenavComponent,
+    SignUpFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
